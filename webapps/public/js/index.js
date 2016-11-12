1e8130e4e4894c5a4ba9ddb1f60c93243a5a7b5e
lastmansleeping <ashishbs7@gmail.com>
2016-11-12 07:07:10 -0800

    Map drawing 1.0

---
+++ webapps/public/js/index.js
@@ -0,0 +1,34 @@
+city = {name: 'City', latitude: -25.363, longitude: 131.044};
+latlng = {lat : -25.363, lng : 131.044};
+function initMap() {
+    //Initialize a map
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: {lat : city['latitude'], lng : city['longitude']}
+    });
+
+    //Plot a marker at the airport of the city
+    drawMarker(map, city)
+
+    //Access API endpoint to get top popular spots
+    //{locations: [{name: 'sds', latitude: 23.32, longitude: 34.43}, {name: 'sds', latitude: 23.32, longitude: 34.43}]}
+
+    //Get distance matrix
+
+    //Order the points
+    //location = city;
+    
+    locations = [{name: 'AAAAA', latitude: -24.363, longitude: 131.044}, {name: 'BBBBB', latitude: -26.363, longitude: 131.044}]
+    //Plot the points
+
+    //Plot the paths
+}
+
+function drawMarker(map, location) {
+    var marker = new google.maps.Marker({
+        position: {lat : location['latitude'], lng : location['longitude']},
+        map: map,
+        title: location['name']
+    });
+
+}
